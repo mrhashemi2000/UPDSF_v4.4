edaed84c398352b2d433ce780446b52f756268bc
Reza Hashemi <mrhashemi2000@gmail.com>
2026-08-07 06:06:57 +0330

    Update UPDSF_v4.4.py

--- UPDSF_v4.4.py
+++ UPDSF_v4.4.py
@@ -41,7 +41,7 @@ REFERENCES:
 AUTHOR: Seyed Mohammad Reza Hashemi (Reza Hashemi) Intelligence-Augmented (IA)
 VERSION: 4.4 with Strictly Empirical(literature-based) Parameters + Lipid Membrane . Parameters for stability and kinetics were derived from
 literature values and adjusted for each environment. An iterative refinement process incorporating theoretical analysis and simulation feedback was employed to optimize
-parameters.
+parameters. UPDSF v4.4 Robustness Check: Testing Thymine dominance under extreme 36x deamination stress (C -> U).
 License & Copyright
 Copyright ©️ 2026 Seyed Mohammad Reza Hashemi  
 This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (CC BY-NC-ND 4.0).
